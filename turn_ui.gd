297970182812595d3ab27db86b287fae2d3c6d18
Gart <gmhobson@gmail.com>
2024-08-20 02:20:13 -0500

    starting enemy ai

--- turn_ui.gd
+++ turn_ui.gd
@@ -2,6 +2,7 @@ extends Panel
 
 var activated = false
 @onready var endTurnButton = $EndTurn
+@onready var label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,8 +13,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	global_position = get_parent().main.camera.global_position + Vector2(256,-128)
 	if activated == true:
-		if visible == false:
+		if get_tree().get_first_node_in_group("BattleScene"):
+			visible = false
+		elif visible == false:
 			visible = true
+		label.text = str(get_parent().turnArray[get_parent().currentTurn].unitClass) + "'s Turn"
 	else:
 		if visible == true:
 			visible = false
